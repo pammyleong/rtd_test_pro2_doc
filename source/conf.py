be822f8068b058f970b15d374aeb620d7cc54b05
pammyleong <pammyleong96@gmail.com>
2024-08-28 17:41:15 +0800

    update confy.py

--- source/conf.py
+++ source/conf.py
@@ -10,6 +10,7 @@ project = 'AmebaPro2\'s Arduino Documentation'
 copyright = '2024 Realtek Semiconductor Corp. All rights reserved'
 author = 'REALTEK SG'
 release = 'v1.0'
+version = '1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -23,6 +24,8 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = []
 
+
+
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
@@ -31,22 +34,3 @@ html_logo = '_static/Realtek_logo.png'
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 numfig = True
-
-# # -- Options for HTML output -------------------------------------------------
-# # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-# html_context = {
-#     "display_github": True,
-#     "github_user": "Ameba-AIoT",
-#     "github_repo": "ameba-arduino-pro2-doc",
-#     "github_version": "main",
-#     "conf_py_path": "/source/",
-# }
-
-# html_logo = '_static/Realtek_logo.png'
-# html_theme = 'sphinx_rtd_theme'
-# html_static_path = ['_static']
-# html_css_files = [
-#     'custom.css',
-# ]
-# numfig = True
