d950e72c20b3d5cdcdf1d0473c8514de15fdc80b
pammyleong <pammyleong96@gmail.com>
2024-08-30 11:23:54 +0800

    update

--- source/conf.py
+++ source/conf.py
@@ -10,7 +10,6 @@ project = 'AmebaPro2\'s Arduino Documentation'
 copyright = '2024 Realtek Semiconductor Corp. All rights reserved'
 author = 'REALTEK SG'
 release = 'v1.0'
-version = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -24,13 +23,21 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-#html_logo = '_static/Realtek_logo.png'
+html_context = {
+    "display_github": True,
+    "github_user": "Ameba-AIoT",
+    "github_repo": "ameba-arduino-pro2-doc",
+    "github_version": "main",
+    "conf_py_path": "/source/",
+}
 
+html_logo = '_static/Realtek_logo.png'
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_css_files = [
+    'custom.css',
+]
 numfig = True
